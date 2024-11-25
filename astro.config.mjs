c3e5026ab4f3828b841d33e7258290ff75eb679c
automator85 <gemacht-agilste0v@icloud.com>
2024-11-25 21:16:50 +0100

    prefetching and error handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: 'https://automator85.github.io',
   base: '/makokobau',
-  integrations: [tailwind()],
-  i18n: {
-    defaultLocale: "de",
-    locales: ["de"],
-    routing: {
-      prefixDefaultLocale: false
-    }
-  }
+  integrations: [tailwind()]
 });
